5a31d1a7adc2fbc119c665bf51031d3b6213d043
Sergio <sergiomunoz1698@gmail.com>
2025-05-31 14:02:58 -0600

    Formulario cliente

--- src/app/components/clientes-form/clientes-form.component.ts
+++ src/app/components/clientes-form/clientes-form.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClientesService } from '../../services/clientes.service';
 
 @Component({
-  selector: 'app-clientes-form',
   standalone: false,
+  selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
-  styleUrl: './clientes-form.component.css'
+  styleUrls: ['./clientes-form.component.css']
 })
-export class ClientesForm {
+export class ClientesForm implements OnInit {
 
+  cliente: any = {
+    nombre: '',
+    telefono: '',
+    direccion: '',
+    estado: true
+  };
+
+  isEdit = false;
+
+  constructor(
+    private clientesService: ClientesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id && id !== '0') {
+      this.isEdit = true;
+      this.clientesService.getCliente(+id).subscribe({
+        next: data => {
+          this.cliente = data;
+        },
+        error: err => {
+          console.error('Error cargando cliente', err);
+          alert('Error al cargar cliente');
+        }
+      });
+    }
+  }
+
+  saveCliente(): void {
+    if (this.isEdit) {
+      this.clientesService.updateCliente(this.cliente.id, this.cliente).subscribe({
+        next: () => {
+          this.router.navigate(['/clientes']);
+        },
+        error: err => {
+          console.error('Error actualizando cliente', err);
+          alert('Error al actualizar cliente');
+        }
+      });
+    } else {
+      this.clientesService.createCliente(this.cliente).subscribe({
+        next: () => {
+          this.router.navigate(['/clientes']);
+        },
+        error: err => {
+          console.error('Error creando cliente', err);
+          alert('Error al crear cliente');
+        }
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/clientes']);
+  }
 }
